89e464df4476457964596b98d8f1fd3cdf6f3306
NimajF <benjafran2000@gmail.com>
2022-03-03 02:23:49 +0100

    Start with about page

--- pages/about.js
+++ pages/about.js
@@ -1,9 +1,46 @@
+import { useState } from "react";
 import Layout from "../components/Layout";
+import styles from "../styles/about.module.css";
+import iconList from "../utils/iconList";
 
-const About = () => (
+const About = () => {
+  return (
     <Layout>
-        <h1>About</h1>
+      <h1 style={{ alignSelf: "flex-start" }}>ABOUT ME</h1>
+      <div className="description">
+        <h3 style={{ fontWeight: "bold" }}>Hey, you got here.</h3>
+        <p style={{ lineHeight: "1.5rem", fontSize: "1.1rem" }}>
+          I am an{" "}
+          <span style={{ color: "rgb(66, 132, 255)", fontWeight: "bold" }}>
+            {" "}
+            autodidact{" "}
+          </span>
+          Front-End developer student. I have a great capacity learning and
+          self-learning, high sense of responsibility and commitment. When faced
+          with a problem, I propose useful solutions and alternatives to
+          continue with the designated task.
+        </p>
+      </div>
+      <div className="text">
+        <h2>Habilties</h2>
+        <p>
+          These are the skills that I am currently improving, gaining
+          knowledge and experience.
+        </p>
+      </div>
+      <section className={styles.skillsContainer}>
+        {iconList.map((icon, idx) => (
+          <div key={idx} className={`${styles.icon}`}>
+            <span className={`${styles[icon.name]}`} >
+              {icon.icon}
+            </span>
+            <p>{icon.name}</p>
+            
+          </div>
+        ))}
+      </section>
     </Layout>
-)
+  );
+};
 
 export default About;
---
+++ utils/iconList.js
@@ -0,0 +1,25 @@
+import {
+  SiHtml5,
+  SiCss3,
+  SiBootstrap,
+  SiJavascript,
+  SiExpress,
+  SiMongodb,
+  SiReact,
+  SiNextdotjs,
+  SiNodedotjs,
+} from "react-icons/si";
+
+const iconList = [
+  { icon: <SiHtml5 />, name: "HTML" },
+  { icon: <SiCss3 />, name: "CSS" },
+  { icon: <SiBootstrap />, name: "Bootstrap" },
+  { icon: <SiJavascript />, name: "JavaScript" },
+  { icon: <SiNodedotjs />, name: "NodeJS" },
+  { icon: <SiExpress />, name: "Express" },
+  { icon: <SiMongodb />, name: "MongoDB" },
+  { icon: <SiReact />, name: "React" },
+  { icon: <SiNextdotjs />, name: "NextJS" },
+];
+
+export default iconList;
