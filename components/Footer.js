694833edf8e2b8cdeccf9569ee36078c6f3cea21
NimajF <benjafran2000@gmail.com>
2022-03-06 05:22:19 +0100

    Add Footer

---
+++ components/Footer.js
@@ -0,0 +1,46 @@
+import Link from "next/link";
+import { socialIcons } from "../utils/iconList";
+
+const Footer = () => (
+  <footer>
+    <div className="footer-links">
+      <ul>
+        <p className="footer-title">Benjamin</p>
+        <li>
+          <Link href="/">
+            <a>Home</a>
+          </Link>
+        </li>
+        <li>
+          <Link href="/about">
+            <a>About</a>
+          </Link>
+        </li>
+        <li>
+          <Link href="/projects">
+            <a>Projects</a>
+          </Link>
+        </li>
+      </ul>
+
+      <ul className="footer-socialIcons">
+        <p className="footer-title">Social</p>
+        {socialIcons
+          .filter((icon) => icon.name !== "Mail")
+          .map((filteredIcon, idx) => (
+            <li key={idx} >
+              <Link href={`${filteredIcon.link}`}>
+                <a>
+                  {filteredIcon.icon}
+                    <> </>
+                  {filteredIcon.name}
+                </a>
+              </Link>
+            </li>
+          ))}
+      </ul>
+    </div>
+    <p>© Benjamin Francisco. All Rights Reserved.</p>
+  </footer>
+);
+export default Footer;
