c30e7ed523d1d76321a31f3a165ec15c97085747
NimajF <benjafran2000@gmail.com>
2022-03-07 03:48:04 +0100

    Move iconList to constants

---
+++ constants/iconList.js
@@ -0,0 +1,50 @@
+import {
+  SiHtml5,
+  SiCss3,
+  SiBootstrap,
+  SiJavascript,
+  SiExpress,
+  SiMongodb,
+  SiReact,
+  SiNextdotjs,
+  SiGit,
+  SiNodedotjs,
+  SiLinkedin,
+  SiGithub,
+  SiMaildotru,
+} from "react-icons/si";
+
+export const iconList = [
+  { icon: <SiHtml5 />, name: "HTML" },
+  { icon: <SiCss3 />, name: "CSS" },
+  { icon: <SiBootstrap />, name: "Bootstrap" },
+  { icon: <SiJavascript />, name: "JavaScript" },
+  { icon: <SiNodedotjs />, name: "NodeJS" },
+  { icon: <SiExpress />, name: "Express" },
+  { icon: <SiMongodb />, name: "MongoDB" },
+  { icon: <SiReact />, name: "React" },
+  { icon: <SiNextdotjs />, name: "NextJS" },
+  { icon: <SiGit />, name: "Git" },
+  { name: "EJS"  }
+];
+
+export const socialIcons = [
+  {
+    icon: <SiLinkedin />,
+    name: "LinkedIn",
+    link: "https://www.linkedin.com/in/bfrancisco12/",
+    linkParam: "linkedin.com/in/brancisco12",
+  },
+  {
+    icon: <SiGithub />,
+    name: "GitHub",
+    link: "https://github.com/NimajF",
+    linkParam: "github.com/NimajF",
+  },
+  {
+    icon: <SiMaildotru />,
+    name: "Mail",
+    link: "benjafran2000@gmail.com",
+    linkParam: "benjafran2000@gmail.com",
+  },
+];
