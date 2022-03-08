3d3da3a57624ec6d787f36bf44f2a5b30622bd5b
NimajF <benjafran2000@gmail.com>
2022-03-08 23:37:58 +0100

    Make navbar active links

--- components/Navbar.js
+++ components/Navbar.js
@@ -1,12 +1,28 @@
 import Link from "next/link";
+import { useRouter } from "next/router";
 
-const Navbar = () => (
+const Navbar = () => {
+  const router = useRouter();
+  const currentRoute = router.pathname;
+
+  return (
     <nav>
-         <Link href="/" ><a>Home</a></Link>   
-         <Link href="/about" ><a>About</a></Link>
-         <Link href="/projects"><a>My Projects</a></Link>
-         <Link href="/contact"><a>Contact me</a></Link>
+      <Link href="/">
+        <a>Benjamin</a>
+      </Link>
+      <div className="nav-links">
+        <Link href="/about">
+          <a className={currentRoute === "/about" ? "active" : "" }>About</a>
+        </Link>
+        <Link href="/projects">
+          <a className={currentRoute === "/projects" ? "active" : ""}>Projects</a>
+        </Link>
+        <Link href="/contact">
+          <a className={currentRoute === "/contactd" ? "active" : ""}>Contact</a>
+        </Link>
+      </div>
     </nav>
-)
+  );
+};
 
 export default Navbar;
