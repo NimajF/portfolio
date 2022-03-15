2da313385db48d373c8cc3ac8c2b36c382d66d43
NimajF <benjafran2000@gmail.com>
2022-03-15 21:17:14 +0100

    Add portfolio as a project

--- constants/projectsData.js
+++ constants/projectsData.js
@@ -26,73 +26,63 @@ const projects = [
       <SiMongodb />,
       <SiNodedotjs />,
     ],
-    completed: "false",
-    screenshot: "/Homepage.png"
+    completed: false,
+    screenshot: "/Homepage.png",
   },
   {
     name: "Colors App",
-    description: "Massive Color App Project",
+    description: "Massive Color App Project.",
     repositoryURL: "https://github.com/NimajF/Colors-App",
     site: "https://nimajf.github.io/Colors-App/",
-    skills: [
-        <SiCss3 />,
-        <SiReact />,
-        <SiReactrouter/>,
-        <SiMaterialui/>
-      ],
-    completed: "true",
-    screenshot: "/ColorsApp.png"
+    skills: [<SiCss3 />, <SiReact />, <SiReactrouter />, <SiMaterialui />],
+    completed: true,
+    screenshot: "/ColorsApp.png",
+  },
+  {
+    name: "Porfolio",
+    description: "Personal Webpage.",
+    repositoryURL: "https://github.com/NimajF/portfolio",
+    site: null,
+    skills: [<SiCss3 />, <SiReact />, <SiNextdotjs />],
+    completed: false,
+    screenshot: "/portfolio.png",
   },
   {
     name: "React nickname generator",
     description: "A React version of my 1st JS/CSS project.",
     repositoryURL: "https://github.com/NimajF/react-nickgenerator",
     site: "https://nimajf.github.io/react-nickgenerator/",
-    skills: [
-        <SiCss3 />,
-        <SiReact />,
-      ],
-    completed: "true",
-    screenshot: "/ReactNickGen.png"
+    skills: [<SiCss3 />, <SiReact />],
+    completed: true,
+    screenshot: "/ReactNickGen.png",
   },
   {
     name: "Simple React Todo-List",
     description:
-      "A simple TodoList w/ useReducer - LocalStorage - React.memo - Material UI",
+      "A simple TodoList w/ useReducer - LocalStorage - React.memo - Material UI.",
     repositoryURL: "https://github.com/NimajF/Simple-React-TodoList",
     site: "https://nimajf.github.io/Simple-React-TodoList/",
-    skills: [
-        <SiCss3 />,
-        <SiReact />,
-        <SiMaterialui/>
-      ],
-    completed: "true",
-    screenshot: "/ReactTodoList.png"
+    skills: [<SiCss3 />, <SiReact />, <SiMaterialui />],
+    completed: true,
+    screenshot: "/ReactTodoList.png",
   },
   {
     name: "DadJokesAPP ",
-    description: "Pitiful Dad-Jokes APP w/ API",
+    description: "Pitiful Dad-Jokes APP w/ API.",
     repositoryURL: "https://github.com/NimajF/DadJokesAPP",
     site: "https://nimajf.github.io/DadJokesAPP/",
-    skills: [
-        <SiCss3 />,
-        <SiReact />,
-      ],
-    completed: "true",
-    screenshot: "/DadJokes.png"
+    skills: [<SiCss3 />, <SiReact />],
+    completed: true,
+    screenshot: "/DadJokes.png",
   },
   {
     name: "NickGenerator",
     description: "A simple nick generator and my very first page!",
     repositoryURL: "https://github.com/NimajF/NickGenerator",
     site: "https://nimajf.github.io/NickGenerator/",
-    skills: [
-        <SiHtml5/>,
-        <SiCss3 />,
-        <SiJavascript />,
-      ],
-    completed: "true",
-    screenshot: "/NickGen.png"
+    skills: [<SiHtml5 />, <SiCss3 />, <SiJavascript />],
+    completed: true,
+    screenshot: "/NickGen.png",
   },
 ];
 
