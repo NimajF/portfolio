45cc463f4ff6282dfc46f31fb3e0709313143fd6
NimajF <benjafran2000@gmail.com>
2022-03-20 03:32:02 +0100

    Add navbar scrolling behaviour

--- components/Navbar.js
+++ components/Navbar.js
@@ -1,19 +1,31 @@
+import { useState, useEffect } from "react";
 import { useRouter } from "next/router";
 import ThemeChanger from "./ThemeChanger";
 import Link from "next/link";
 
 const Navbar = () => {
-  // const [click, setClick] = useState();
-
-  // const handleClick = () => {
-
-  // }
+  const [sticky, setSticky] = useState("relative");
 
   const router = useRouter();
   const currentRoute = router.pathname;
 
+  useEffect(() => {
+    window.addEventListener("scroll", stickNavbar);
+
+    return () => {
+      window.removeEventListener("scroll", stickNavbar);
+    };
+  }, []);
+
+  const stickNavbar = () => {
+    if (window !== undefined) {
+      let windowHeight = window.scrollY;
+      windowHeight > 200 ? setSticky("stickyNavbar") : setSticky("relative");
+    }
+  };
+
   return (
-    <nav>
+    <nav className={sticky}>
       <Link href="/">
         <div className="personal-logo">
           <h4>BENJAMIN</h4>
@@ -21,9 +33,6 @@ const Navbar = () => {
         </div>
       </Link>
       <ThemeChanger />
-      {/* <button className="theme-button">
-        <BsFillMoonFill />
-      </button> */}
       <div className="nav-links">
         <Link href="/about">
           <a className={currentRoute === "/about" ? "active" : ""}>About</a>
