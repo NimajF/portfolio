c3f3e9cc87b13d03e134c52034a1061c5433a084
NimajF <benjafran2000@gmail.com>
2022-03-24 20:41:47 +0100

    404 page minor changes

--- pages/404.js
+++ pages/404.js
@@ -1,17 +1,41 @@
+import Head from "next/dist/shared/lib/head";
 import Link from "next/link";
 import Navbar from "../components/Navbar";
 
 const notFound = () => (
-   <>
-    <Navbar/>
-  <div style={{ display: "flex", flexDirection: "column", justifyContent: "center", alignItems: "center" }} >
-    <h1>Error 404</h1>
-    <h2>Ups...</h2>
-    <p>This page does not exist.</p>
-    <Link href="/">
-      <button className="link-button" style={{ fontSize: "1.1rem" }} >Home</button>
-    </Link>
-  </div>
-   </>
+  <>
+    <Head>
+      <title>Page Not Found - Benjamin</title>
+    </Head>
+    <Navbar />
+    <div className="description" style={{ marginTop: "10rem" }}>
+      <div className="header-info">
+        <h1
+          style={{
+            alignSelf: "center",
+            width: "50%",
+            fontSize: "4rem",
+            alignSelf: "center",
+          }}
+        >
+          Ups! Error 404
+        </h1>
+        <h2>This page does not exist.</h2>
+        <p className="p-text">Please return back.</p>
+        <Link href="/">
+          <button
+            className="link-button"
+            style={{
+              fontSize: "1.1rem",
+              marginTop: "1rem",
+              alignSelf: "center",
+            }}
+          >
+            Home
+          </button>
+        </Link>
+      </div>
+    </div>
+  </>
 );
 export default notFound;
