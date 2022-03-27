3ca55acb32efd490f438c51dde47483e08d935bd
NimajF <benjafran2000@gmail.com>
2022-03-27 23:14:22 +0200

    Add page transitions

--- pages/_app.js
+++ pages/_app.js
@@ -1,11 +1,28 @@
 import { ThemeProvider } from "next-themes";
+import { motion } from "framer-motion";
 import "../styles/globals.css";
 
-function MyApp({ Component, pageProps }) {
+const pageTransition = {
+  pageInitial: {
+    opacity: 0,
+  },
+  pageAnimate: {
+    opacity: 1,
+  },
+};
+
+function MyApp({ Component, pageProps, router }) {
   return (
-    <ThemeProvider>
-      <Component {...pageProps} />
-    </ThemeProvider>
+    <motion.div
+      key={router.route}
+      initial="pageInitial"
+      animate="pageAnimate"
+      variants={pageTransition}
+    >
+      <ThemeProvider>
+        <Component {...pageProps} />
+      </ThemeProvider>
+    </motion.div>
   );
 }
 
