c711b52b3f339a31ff19493c58e8518bfbec085e
NimajF <benjafran2000@gmail.com>
2022-03-29 00:26:38 +0200

    Add motionConstants

---
+++ constants/motionConstants.js
@@ -0,0 +1,49 @@
+import { motion } from "framer-motion";
+
+const hiddenText = {
+  hidden: {
+    scale: 0.8,
+    opacity: 0,
+  },
+  visible: {
+    scale: 1,
+    opacity: 1,
+    transition: {
+      delay: 0.5,
+    },
+  },
+};
+
+const container = {
+  hidden: { opacity: 1, scale: 0 },
+  visible: {
+    opacity: 1,
+    scale: 1,
+    transition: {
+      delayChildren: 2.5,
+      staggerChildren: 0.2,
+    },
+  },
+};
+
+const contactContainer = {
+  hidden: { opacity: 1, scale: 0 },
+  visible: {
+    opacity: 1,
+    scale: 1,
+    transition: {
+      delayChildren: 1,
+      staggerChildren: 0.5,
+    },
+  },
+};
+
+const item = {
+  hidden: { y: 20, opacity: 0 },
+  visible: {
+    y: 0,
+    opacity: 1,
+  },
+};
+
+export { hiddenText, container, contactContainer, item };
