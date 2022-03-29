81b97f7de5019ca6d6e6be1407743ae9a0fae112
NimajF <benjafran2000@gmail.com>
2022-03-29 23:37:03 +0200

    Fix overflow bug

--- components/Layout.js
+++ components/Layout.js
@@ -3,7 +3,7 @@ import Footer from "./Footer";
 const Layout = ({ children }) => (
   <>
     <Navbar />
-    <main className="mainContainer">
+    <main className="mainContainer" style={{ overflowX: "hidden" }}>
       <div className="container">{children}</div>
     </main>
     <Footer />
