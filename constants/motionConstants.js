05b4266dbc98f8db79ac7c8f89827de7602036bf
NimajF <benjafran2000@gmail.com>
2022-03-29 23:37:40 +0200

    Add more constants

--- constants/motionConstants.js
+++ constants/motionConstants.js
@@ -1,6 +1,4 @@
-import { motion } from "framer-motion";
-
-const hiddenText = {
+const hiddenTitle = {
   hidden: {
     scale: 0.8,
     opacity: 0,
@@ -14,14 +12,43 @@ const hiddenText = {
   },
 };
 
-const container = {
-  hidden: { opacity: 1, scale: 0 },
+const hiddenSubTitle = {
+  hidden: {
+    scale: 0.8,
+    opacity: 0,
+  },
   visible: {
-    opacity: 1,
     scale: 1,
+    opacity: 1,
     transition: {
-      delayChildren: 2.5,
-      staggerChildren: 0.2,
+      delay: 1,
+    },
+  },
+};
+
+const hiddenText = {
+  hidden: {
+    opacity: 0,
+    x: 100,
+  },
+  visible: { opacity: 1, x: 0, transition: { duration: 0.5, delay: 1 } },
+};
+
+// initial={{ x: 10, opacity: 0 }}
+// animate={{ x: 0, opacity: 1 }}
+// exit={{ x: -300, opacity: 0 }}
+// transition={{ duration: 0.5, delay: 1 }}
+
+const container = {
+  hidden: {
+    scale: 0.8,
+    opacity: 0,
+  },
+  visible: {
+    scale: 1,
+    opacity: 1,
+    transition: {
+      delay: 2,
     },
   },
 };
@@ -46,4 +73,11 @@ const item = {
   },
 };
 
-export { hiddenText, container, contactContainer, item };
+export {
+  hiddenTitle,
+  hiddenSubTitle,
+  hiddenText,
+  container,
+  contactContainer,
+  item,
+};
